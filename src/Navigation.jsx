59b3c6a1bff1b8b92d822d38af43257e30c48f6b
Scott Goodrow <sgoodrow@gmail.com>
2021-02-08 00:26:45 -0800

    feat: Add github link.

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -3,14 +3,20 @@ import {
 } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import {
-  Brightness3, Brightness7,
+  Brightness3, Brightness7, GitHub,
 } from '@material-ui/icons';
 import React from 'react';
 
 export default function Navigation({ dark, setDark }) {
+  const handleClickGithub = () => {
+    window.location.href = 'https://github.com/sgoodrow/timerly/';
+  };
+
   const handleThemeChange = () => {
     setDark((d) => !d);
   };
+
+  const style = { color: 'white' };
 
   return (
     <AppBar position="static">
@@ -21,13 +27,22 @@ export default function Navigation({ dark, setDark }) {
           </Typography>
         </Box>
         <IconButton
+          aria-label="git"
+          onClick={handleClickGithub}
+        >
+          {dark
+            ? <GitHub />
+            : <GitHub style={style} />}
+        </IconButton>
+        <IconButton
           aria-label="theme"
           onClick={handleThemeChange}
         >
-          {dark ? <Brightness3 /> : <Brightness7 style={{ color: 'white' }} />}
+          {dark
+            ? <Brightness3 />
+            : <Brightness7 style={style} />}
         </IconButton>
       </Toolbar>
-
     </AppBar>
   );
 }
